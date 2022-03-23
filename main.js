ffac9750ef382fca071629c67ac8aca468dfe89f
Aqua <99206531+WedleySilva@users.noreply.github.com>
2022-03-23 14:38:29 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,13 +1,12 @@
-
 function multiplicar(valor1, valor2, valor3) {
-    return valor1 * valor2 * valor3
+    return  valor1 * valor2 * valor3
 }
 function somar(valor1, valor2, valor3) {
     return valor1 + valor2 + valor3
 }
 
 function dividir(valor1, valor2, valor3) {
-    return valor1 / valor2 / valor3
+    return  valor1 / valor2 / valor3
 }
 
 function subtrair(valor1, valor2, valor3) {
@@ -17,28 +16,28 @@ function subtrair(valor1, valor2, valor3) {
 function handleBtMult() {
     const v1 = Number(document.getElementById('valor1').value)
     const v2 = Number(document.getElementById('valor2').value)
-    const valor3 = Number(document.getElementById('valor3').value)
+    const v3 = Number(document.getElementById('valor3').value)
     document.getElementById('resultado').innerHTML = multiplicar(v1, v2, v3)
 }
 
 function handleBtSomar () {
     const v1 = Number(document.getElementById('valor1').value)
     const v2 = Number(document.getElementById('valor2').value)
-    const valor3 = Number(document.getElementById('valor3').value)
+    const v3 = Number(document.getElementById('valor3').value)
     document.getElementById('resultado').innerHTML = somar(v1, v2, v3)
 }
 
 function handleBtSub () {
     const v1 = Number(document.getElementById('valor1').value)
     const v2 = Number(document.getElementById('valor2').value)
-    const valor3 = Number(document.getElementById('valor3').value)
+    const v3 = Number(document.getElementById('valor3').value)
     document.getElementById('resultado').innerHTML = subtrair(v1, v2, v3)
 }
 
 function handleBtDiv () {
     const v1 = Number(document.getElementById('valor1').value)
     const v2 = Number(document.getElementById('valor2').value)
-    const valor3 = Number(document.getElementById('valor3').value)
+    const v3 = Number(document.getElementById('valor3').value)
     document.getElementById('resultado').innerHTML = dividir(v1, v2, v3)
 }
 
